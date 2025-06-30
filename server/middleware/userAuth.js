65178ff01b02382bdb6873a6df8dc4c7ed2c1fbd
ZOUHAIR <zouhaireddoubaji497@gmail.com>
2025-06-30 13:26:14 +0100

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,25 +1,4 @@
-// import jwt from "jsonwebtoken";
 
-// const userAuth = (req, res, next) => {
-//     const { token } = req.cookies;
-//     if (!token) {
-//         return res.json({ success: false, message: "Unauthorized" });
-//     }
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         if (decoded.id) {
-//             req.body.userId = decoded.id;
-//             return next();
-//         }else {
-//             return res.json({ success: false, message: "Unauthorized login again" });
-//         }
-//     } catch (error) {
-//         return res.json({ success: false, message: "Invalid token" });
-//     }
-
-// }
-
-// export default userAuth;
 
 import jwt from "jsonwebtoken";
 
